0818fecea67367d1737a3584627f90d659cdf26f
Vunny Sodhi <vunny.sodhi@outlook.com>
2025-08-08 16:28:19 +0300

    Use latest Qemu
    Signed-off-by: Vunny Sodhi <vunny.sodhi@outlook.com>

--- pkgs/applications/virtualization/qemu/default.nix
+++ pkgs/applications/virtualization/qemu/default.nix
@@ -146,11 +146,11 @@ stdenv.mkDerivation (finalAttrs: {
     + lib.optionalString nixosTestRunner "-for-vm-tests"
     + lib.optionalString toolsOnly "-utils"
     + lib.optionalString userOnly "-user";
-  version = "10.0.2";
+  version = "10.1.0-rc2";
 
   src = fetchurl {
     url = "https://download.qemu.org/qemu-${finalAttrs.version}.tar.xz";
-    hash = "sha256-73hvI5jLUYRgD2mu9NXWke/URXajz/QSbTjUxv7Id1k=";
+    hash = "sha256-/UaK4rnroO9XUfI5OCpOhOmi6Oe3I/Ps05jIwZ+HmyM=";
   };
 
   depsBuildBuild = [
@@ -296,6 +296,8 @@ stdenv.mkDerivation (finalAttrs: {
       --replace '$source_path/VERSION' '$source_path/QEMU_VERSION'
     substituteInPlace meson.build \
       --replace "'VERSION'" "'QEMU_VERSION'"
+    substituteInPlace docs/conf.py \
+      --replace-fail "'../VERSION'" "'../QEMU_VERSION'"
     substituteInPlace python/qemu/machine/machine.py \
       --replace-fail /var/tmp "$TMPDIR"
   '';
